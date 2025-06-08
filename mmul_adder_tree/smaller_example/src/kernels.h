fad57af967d742c1ed090f2455cad4f0eb97194d
Gram K. <148499819+GramKoski@users.noreply.github.com>
2025-06-08 14:36:55 -0700

    Update kernels.h comments.

--- mmul_adder_tree/smaller_example/src/kernels.h
+++ mmul_adder_tree/smaller_example/src/kernels.h
@@ -4,40 +4,45 @@
 
 using namespace adf;
 
+// Matrix multiplication kernel class wrapper for "skinny" matrices (one dimension much larger)
 class mmul_skinny {
-    int K;
-    int M;
-    int T;
-    int a_block;
+    int K;       // Inner dimension (columns of A / rows of B)
+    int M;       // Rows of A and C
+    int T;       // Columns of B and C
+    int a_block; // Block ID for parallel processing
 public:
-    mmul_skinny(int K_val, int M_val, int T_val, int id) :  K(K_val), M(M_val), T(T_val), a_block(id) {}
+    mmul_skinny(int K_val, int M_val, int T_val, int id) 
+        : K(K_val), M(M_val), T(T_val), a_block(id) {}
 
     void run(adf::input_buffer<int16>& a_buf,
              adf::input_buffer<int16>& b_buf,
              adf::output_buffer<int16>& c_buf);
 
+    // Registers kernel function and parameters with the ADF runtime
     static void registerKernelClass() {
         REGISTER_FUNCTION(mmul_skinny::run);
-	REGISTER_PARAMETER(K);
-	REGISTER_PARAMETER(M);
-	REGISTER_PARAMETER(T);
+        REGISTER_PARAMETER(K);
+        REGISTER_PARAMETER(M);
+        REGISTER_PARAMETER(T);
         REGISTER_PARAMETER(a_block);
     }
 };
 
+// 4-input addition tree kernel (element-wise sum of 4 vectors)
 class add_tree_4 {
-    int M;
+    int M; // Vector length
 public:
-    
-    add_4(int M_val) : M(M_val) {} 
+    add_tree_4(int M_val) : M(M_val) {}
+
     void run(
-    adf::input_buffer<int16>& in0, 
-    adf::input_buffer<int16>& in1,  
-    adf::input_buffer<int16>& in2,
-    adf::input_buffer<int16>& in3,
-    adf::output_buffer<int16>& out);
+        adf::input_buffer<int16>& in0, 
+        adf::input_buffer<int16>& in1,  
+        adf::input_buffer<int16>& in2,
+        adf::input_buffer<int16>& in3,
+        adf::output_buffer<int16>& out);
+
     static void registerKernelClass() {
-        REGISTER_FUNCTION(add_4::run);
+        REGISTER_FUNCTION(add_tree_4::run);
         REGISTER_PARAMETER(M);
     }
 };
